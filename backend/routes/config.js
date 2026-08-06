31c4c23cfc2915491e74864eb45d67a5fa747d1a
taiyabj-creator <taiyab.soumya.jamal@gmail.com>
2026-08-06 17:39:48 +0530

    Production hardening and deployment preparation

--- backend/routes/config.js
+++ backend/routes/config.js
@@ -6,12 +6,17 @@ const path = require("path");
 
 const configPath = path.join(__dirname, "../config/plant.json");
 
+const { authMiddleware } = require("../middleware/auth");
+
 router.get("/plant-config", (req, res) => {
   const data = JSON.parse(fs.readFileSync(configPath, "utf8"));
   res.json(data);
 });
 
-router.put("/plant-config", (req, res) => {
+router.put(
+  "/plant-config",
+  authMiddleware,
+  (req, res) => {
   fs.writeFileSync(
     configPath,
     JSON.stringify(req.body, null, 2)
